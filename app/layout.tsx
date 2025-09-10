4763c936678dddb1e375d685a0a9ceff25874407
lucidio <jubiladocantv@gmail.com>
2025-09-09 21:47:11 -0400

    feat: Set default theme to light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
       <body className="antialiased">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
