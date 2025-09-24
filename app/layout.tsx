a51431b61d254721871ad12bf205823bbcc64649
lucidio <jubiladocantv@gmail.com>
2025-09-24 10:08:12 -0400

    feat: Integra Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type React from "react"
 import type { Metadata } from "next"
 import { Montserrat } from "next/font/google"
@@ -86,6 +87,7 @@ export default function RootLayout({
           <div id="main-content">{children}</div>
           <AccessibilityToolbar />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
