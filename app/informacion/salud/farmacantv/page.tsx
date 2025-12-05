d5ace435207756462f202fa4f66213410a11ff40
lucidio <jubiladocantv@gmail.com>
2025-12-05 15:13:49 -0400

    feat: Actualiza imagen de FarmaCantv y elimina publicidad pasada

--- app/informacion/salud/farmacantv/page.tsx
+++ app/informacion/salud/farmacantv/page.tsx
@@ -30,7 +30,7 @@ export default function FarmacantvPage() {
           <div className="flex flex-col gap-8">
             <div className="bg-card p-4 sm:p-6 rounded-lg shadow-md flex justify-center">
               <Image
-                src="/farmacantv.jpg"
+                src="/FarmaCantv1.png"
                 alt="Información sobre Farmacantv"
                 width={800}
                 height={1120}
